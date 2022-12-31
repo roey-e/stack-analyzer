f80a3452e1b120013e416b3c74632a032b475d10
roey-e <lrteroe@gmail.com>
2022-12-31 18:06:22 +0200

    formatting

--- stack_analyzer/util.py
+++ stack_analyzer/util.py
@@ -1,9 +1,11 @@
 import typing
 
+
 def is_type_optional(tp: typing.Type) -> bool:
     if typing.get_origin(tp) is typing.Union:
         args = typing.get_args(tp)
         return len(args) == 2 and type(None) in args
+
 
 def get_optional_arg(tp: typing.Type[typing.Optional[typing.Type]]) -> typing.Type:
     if not is_type_optional(tp):
